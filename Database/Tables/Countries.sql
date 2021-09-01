4889115fe8291d310b034c3db316db3f1f9b44d7
emilskovmand <emilskovmand@gmail.com>
2021-09-01 19:26:55 +0200

    Initial database

---
+++ Database/Tables/Countries.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Countries]
+(
+	[CountryId] INT NOT NULL,
+	[OfficialName] NVARCHAR(MAX),
+	[ShortName] NVARCHAR(256),
+	[CountryCode] NVARCHAR(128),
+	[CountryInitials] NVARCHAR(10),
+	CONSTRAINT [PK_dbo.Country] PRIMARY KEY CLUSTERED ([CountryId] ASC)
+)
---
+++ Database/Tables/CustomerUser.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[CustomerUser]
+(
+	[Id] NVARCHAR(128) NOT NULL,
+	[FirstName] nvarchar(256) NOT NULL,
+	[LastName] nvarchar(256) NOT NULL,
+	[Email] NVARCHAR(256) NOT NULL,
+	[NormalizedEmail] NVARCHAR(256) NOT NULL,
+	[PasswordHash] NVARCHAR(MAX) NULL,
+	[TwoFactorEnabled] BIT NOT NULL DEFAULT 0,
+	[DateOfCreation] DATETIME2 NOT NULL DEFAULT getdate(),
+	[PhoneNumber] NVARCHAR(MAX) NULL,
+	[PhoneNumberConfirmed] BIT NOT NULL DEFAULT 0,
+	CONSTRAINT [PK_dbo.CustomerUser] PRIMARY KEY CLUSTERED ([Id] ASC),
+)
